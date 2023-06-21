f8b385f9cc3d82cc3923972509cc426b6b78e779
Rob Runka <rob.runka@golfaday.com>
2023-06-20 21:32:00 -0600

    commit to test cicd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,6 +51,7 @@ const Homepage = () => {
             <div>
               <div className="mt-10">
                 {/* Decorative image grid */}
+
                 <div
                   aria-hidden="true"
                   className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
